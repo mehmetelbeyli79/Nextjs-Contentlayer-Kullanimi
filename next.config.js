2ce361a60737fd5d1418473fcc2407be138290ff
mehmetelbeyli79 <mehmetelbeyli88@gmail.com>
2022-12-08 13:56:38 +0300

    thml-react-parser ve içerik görüntüleme gerçekleş

--- next.config.js
+++ next.config.js
@@ -1,16 +1,27 @@
+/** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+  
+// }
+// module.exports = nextConfig
 const { withContentlayer } = require("next-contentlayer");
 
 module.exports = withContentlayer({
-  reactStrictMode: true,
-  images: {
-    domains: [
-      "images.unsplash.com",
-      "dl.airtable.com",
-      "v5.airtableusercontent.com",
-    ],
-  },
-  staticPageGenerationTimeout: 10,
-  experimental: {
-    appDir: true,
-  },
+  // reactStrictMode: true,
+  // images: {
+  //   domains: [
+  //     "images.unsplash.com",
+  //     "dl.airtable.com",
+  //     "v5.airtableusercontent.com",
+  //   ],
+  // },
+  // staticPageGenerationTimeout: 10,
+  // experimental: {
+  //   appDir: true,
+  // },
 });
+// import { withContentlayer } from 'next-contentlayer'
+
+// // module.exports=withContentlayer({});
+
+// export default withContentlayer({})
--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useRouter } from "next/router";
 import { allPosts } from 'contentlayer/generated'
+import parse from 'html-react-parser';
 
 
 
@@ -9,11 +10,13 @@ function Icerik() {
   const icerik=router.query.postId;
   const makale=allPosts.filter((post)=>post.url===("/posts/"+icerik));
   console.log(makale);
+  const eleman=makale[0].body.html;
+
   return (
 
-    <div>
-      <h2>{makale[0].title}</h2>
-      <p>{makale[0].body.html}</p>
+    <div className='flex flex-col m-6'>
+      <h2 className='text-2xl'>{makale[0].title}</h2>
+      {parse(eleman)}
       <p>{makale[0].date}</p>
     </div>
   )
